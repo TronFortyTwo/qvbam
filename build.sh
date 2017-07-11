21f43895dcb83591d75a42fb9d5c66df93404fe9
TronFourtyTwo <emanuele.sorce@hotmail.com>
2017-07-11 12:35:57 +0200

    New logo, fixed building on modern compilers

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ if [[ ! -f lib/SDL-1.2.15/build/.libs/libSDL.a ]]; then
 	tar xf SDL-1.2.15.tar.gz
 	cd SDL-1.2.15/
 	patch  -p 1 -f < ../SDL1.patch
-	./autogen.sh && ./configure --enable-input-tslib=off && make -j4
+	./autogen.sh && ./configure --enable-input-tslib=off && make -j2
 	cd ../..
 fi
 mkdir build
 cd build
 cmake .. -DSDL_PATH=`pwd`/../lib/SDL-1.2.15/
-make
+make -j2
 
--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 mkdir package
-cp build/qvbam build/qml logo.png manifest.json qvbam.desktop qvbam.json ./package -rf
+cp build/qvbam build/qml logo.svg manifest.json qvbam.desktop qvbam.json ./package -rf
 cd package
 click build .
 cp *.click ..
