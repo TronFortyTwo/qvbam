a2784f3b9e92c2b4afbe2bf09443e351ab7b7482
TronFortyTwo <emanuele.sorce@hotmail.com>
2018-02-24 00:35:25 +0100

    Improved make process

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ if [[ ! -f lib/SDL-1.2.15/build/.libs/libSDL.a ]]; then
 	cd lib k
 	tar xf SDL-1.2.15.tar.gz
 	cd SDL-1.2.15/
-		patch  -p 1 -f < ../SDL1.patch
-	./autogen.sh && ./configure --enable-input-tslib=off && make -j4
+	patch  -p 1 -f < ../SDL1.patch
+	./autogen.sh && ./configure --enable-input-tslib=off CXXFLAGS="-O3" CFLAGS="-O3" && make -j4
 	cd ../..
 fi
 mkdir build
