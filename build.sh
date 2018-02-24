b689c653f4027226124f6b80705668417380b8e1
TronFortyTwo <emanuele.sorce@hotmail.com>
2018-02-24 01:02:11 +0100

    Updated build script

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+export MAKEFLAGS=-j$(($(grep -c ^processor /proc/cpuinfo) - 0))
+
 if [[ ! -f lib/SDL-1.2.15/build/.libs/libSDL.a ]]; then
 	cd lib k
 	tar xf SDL-1.2.15.tar.gz
 	cd SDL-1.2.15/
 	patch  -p 1 -f < ../SDL1.patch
-	./autogen.sh && ./configure --enable-input-tslib=off CXXFLAGS="-O3" CFLAGS="-O3" && make -j4
+	./autogen.sh && ./configure --enable-input-tslib=off CXXFLAGS="-O3" CFLAGS="-O3" && make
 	cd ../..
 fi
 mkdir build
 cd build
 cmake .. -DSDL_PATH=`pwd`/../lib/SDL-1.2.15/
-make -j4
+make
 
