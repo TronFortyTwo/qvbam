3a8ef45a2a992d93b55ae6debbb80253dcc4ba0d
tronfortytwo <emanuele.sorce@hotmail.com>
2018-10-21 11:22:34 +0200

    Improved build script

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ export CC="clang"
 export CXX="g++"
 
 if [[ ! -f lib/SDL-1.2.15/build/.libs/libSDL.a ]]; then
-	cd lib k
+	cd lib
 	tar xf SDL-1.2.15.tar.gz
 	cd SDL-1.2.15/
 	patch  -p 1 -f < ../SDL1.patch
-	./autogen.sh && ./configure --enable-input-tslib=off CXXFLAGS="-O3 -std=gnu++11" CFLAGS="-O3" && make
+	./autogen.sh && ./configure --enable-input-tslib=off CXXFLAGS="-Ofast -std=gnu++11" CFLAGS="-Ofast" && make
 	cd ../..
 fi
 mkdir build
